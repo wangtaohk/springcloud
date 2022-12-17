8470f5f22efae5af87057e9a2c76f4a99c5d0cd0
tao <wangtao_jobs@163.com>
2022-12-17 17:28:58 +0800

    提交SpringCloud教程6-7章节代码 6.分布式配置中心 7.高可用的分布式配置中心

---
+++ config/eureka-config-server/src/main/java/com/forezp/eurekaconfigserver/EurekaConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.forezp.eurekaconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConfigServerApplication.class, args);
+    }
+
+}
---
+++ config/eureka-config-server/src/test/java/com/forezp/eurekaconfigserver/EurekaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forezp.eurekaconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
